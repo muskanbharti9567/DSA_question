239c77edaa196298b9f6d439cd4e121bfad4720f
Muskan Bharti <muskanbharti9567@gmail.com>
2026-06-30 23:49:33 +0530

    Time: 4 ms (39.92%) | Memory: 12.1 MB (11.01%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int>need;
+        unordered_map<char,int>have;
+
+        for(int i=0; i<ransomNote.size(); i++){
+            need[ransomNote[i]]++;
+        }
+        for(int i=0; i<magazine.size(); i++){
+            have[magazine[i]]++;
+        }
+
+        for(auto i : need){
+            char c = i.first;
+            int fneed = i.second;
+            int fhave = have[c];
+
+            if(fneed>fhave){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
