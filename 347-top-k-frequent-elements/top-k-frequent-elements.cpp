aad84e459d667037574dfe5f6e89889a5429a65e
Muskan Bharti <muskanbharti9567@gmail.com>
2026-07-31 23:03:21 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (22.08%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,44 +1,25 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int>mp;
 
-        vector<pair<int,int>> freq;
-
-        for (int i = 0; i < nums.size(); i++) {
-
-            bool alreadyCounted = false;
-
-            // Check whether nums[i] appeared before
-            for (int j = 0; j < i; j++) {
-                if (nums[j] == nums[i]) {
-                    alreadyCounted = true;
-                    break;
-                }
-            }
-
-            if (alreadyCounted)
-                continue;
-
-            // Count frequency of nums[i]
-            int count = 0;
-
-            for (int j = 0; j < nums.size(); j++) {
-                if (nums[j] == nums[i]) {
-                    count++;
-                }
-            }
-
-            freq.push_back({count, nums[i]});
+        for (int num : nums) {
+            mp[num]++;
         }
 
-        sort(freq.rbegin(), freq.rend());
+        vector<pair<int,int>>freq;
 
-        vector<int> ans;
+        for(auto it : mp){
+            freq.push_back({it.second,it.first});
+        }
+        sort(freq.rbegin(),freq.rend());
 
-        for (int i = 0; i < k; i++) {
+        vector<int>ans;
+        for(int i=0; i<k; i++){
             ans.push_back(freq[i].second);
         }
-
         return ans;
+
+        
     }
 };
