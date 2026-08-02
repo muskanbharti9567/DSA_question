e484148dd378b3b0604a5bf85e33d44c140137ea
Muskan Bharti <muskanbharti9567@gmail.com>
2026-08-02 12:11:22 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (19.27%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int unique=0;
+        int i = 0;
+        int j = i+1;
+        while(j<nums.size()){
+            if(nums[i]==nums[j]){
+                j++;
+            }
+            else{
+                nums[i+1]=nums[j];
+                i++;
+                j++;
+                unique ++;
+            }
+        }
+        return unique+1;
+        
+    }
+};
