8a2561f05fd4a0d6c9b06ef3612e65b1e060a992
Muskan Bharti <muskanbharti9567@gmail.com>
2026-08-02 21:08:19 +0530

    Time: 72 ms (68.51%) | Memory: 131.5 MB (30.51%) - LeetSync

---
+++ 502-ipo/ipo.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        int n = profits.size();
+
+        vector<pair<int,int>>proj;
+        for(int i=0; i<n; i++){
+            proj.push_back({capital[i],profits[i]});
+            // {0,1} {1,2} {1,3}
+        }
+        sort(proj.begin(),proj.end());
+        priority_queue<int>pq;
+        
+        int indx = 0;
+        while(k--){
+            while(indx<n){
+                if(proj[indx].first>w){
+                   break ; 
+                }
+                pq.push(proj[indx].second);
+                indx++;
+            }
+            if(pq.empty()) break;
+            
+            w = w+pq.top();
+            pq.pop();
+        }
+        return w;
+    }
+};
