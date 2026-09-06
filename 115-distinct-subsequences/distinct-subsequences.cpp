2a62f6db816798ca4d392c554bd3d4729c763a33
Muskan Bharti <muskanbharti9567@gmail.com>
2026-09-06 22:45:19 +0530

    Time: 3 ms (99.83%) | Memory: 12.6 MB (81.13%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,21 +1,37 @@
+unsigned dp[1001][1001]; // dp[i][j] denotes # for t[0:j] in s[0:i]
 class Solution {
-private:
-    int f(int i , int j, string& s, string& t, int n1, int n2, vector<vector<int>>& dp){
-        if(j == n2) return 1;
-        if(i == n1) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s[i] == t[j]){
-            return dp[i][j] = f(i + 1, j + 1, s, t, n1, n2, dp)
-                 + f(i + 1, j, s, t, n1, n2, dp);
-        }
-        return dp[i][j] = f(i + 1, j, s, t, n1, n2, dp);
-    }
-
 public:
-    int numDistinct(string s, string t) {
-        int n1 = s.size();
-        int n2 = t.size();
-        vector<vector<int>> dp(n1, vector<int>(n2, -1));
-        return f(0, 0, s, t, n1, n2, dp);
+    int m, n;
+    string_view s, t;
+    unsigned f(int i, int j) {
+        if (j==0) return 1; // base case
+        else if (i==0 || j>i) return 0;
+        if (dp[i][j] != UINT_MAX) return dp[i][j];
+        
+        unsigned ans=0;
+        if (s[i-1]==t[j-1]) 
+            ans=f(i-1, j-1) + f(i-1, j);
+        else
+            ans=f(i-1, j);
+        
+        return dp[i][j]=ans;
+    }
+    
+    int numDistinct(string& s, string& t) {
+        this->s = s;
+        this->t = t;
+        m=s.size();
+        n=t.size();
+        for(int i=0; i<=m; i++)
+            memset(dp[i], -1, (n+1)*sizeof(unsigned));
+        return f(m, n);
     }
 };
+
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
